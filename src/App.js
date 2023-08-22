fe2cb8adcf4466ae1d8a2d8127111cdc83d03616
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-08-22 15:53:47 +0400

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages/pages";
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact patch="/">
+          <Home />
+        </Route>
+        <Route patch="/about">
+          <About />
+        </Route>
+        <Route patch="/cart">
+          <Cart />
+        </Route>
+        <Route patch="/products">
+          <Products />
+        </Route>
+        <Route patch="/products/:id" children={<SingleProduct />}></Route>
+        <Route patch="/checkout">
+          <Checkout />
+        </Route>
+        <Route patch="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
