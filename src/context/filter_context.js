9b9dcdf7fc47892ea4fdc62c4fd8b013e538c367
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-08-30 15:00:16 +0400

    work with Filters Shipping and Clear Filters

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -64,9 +64,14 @@ export const FilterProvider = ({ children }) => {
     if (name === "price") {
       value = Number(value);
     }
+    if (name === "shipping") {
+      value = e.target.checked;
+    }
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
   };
-  const clearFilters = () => {};
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS });
+  };
   return (
     <FilterContext.Provider
       value={{
@@ -75,6 +80,7 @@ export const FilterProvider = ({ children }) => {
         setListView,
         updateSort,
         updateFilters,
+        clearFilters,
       }}
     >
       {children}
