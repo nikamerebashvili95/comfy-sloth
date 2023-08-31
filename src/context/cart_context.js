7565c0d55618460feafbc0febb4e051f3d1d8081
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-08-31 16:41:04 +0400

    work with Cart Calculate Totals

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -46,6 +46,7 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: CLEAR_CART });
   };
   useEffect(() => {
+    dispatch({ type: COUNT_CART_TOTALS });
     localStorage.setItem("cart", JSON.stringify(state.cart));
   }, [state.cart]);
   return (
